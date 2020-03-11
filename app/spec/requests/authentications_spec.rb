8ce3cd2ca675fe83e6440d85002b4bca0721f625
kazufusa <kazu.jiyuunosenshi.fusa@gmail.com>
2020-03-11 14:54:28 +0900

    refactor(test): use shared_examples in request test

--- app/spec/requests/authentications_spec.rb
+++ app/spec/requests/authentications_spec.rb
@@ -1,172 +1,91 @@
 require 'rails_helper'
 
-describe 'Sign up with password', type: :request do
+RSpec.shared_examples 'A Ruby object with 4 elements' do |with_pw, expired|
   before(:each) do
     @current_user = FactoryBot.build_stubbed(:user)
     params = {
       email: @current_user.email,
-      password: @current_user.password,
+      password: with_pw ? @current_user.password : nil,
       confirm_success_url: "https://testapp.com/registration"
     }
     post(user_registration_path, params: params)
   end
 
-  context "and confirm and sign in" do
-    it "gives you an new user" do
-      expect(response).to have_http_status(:success)
-    end
+  it "gives you an new user" do
+    expect(response).to have_http_status(:success)
+  end
 
-    it "gives you an confirmation mail" do
-      expect(ActionMailer::Base.deliveries.last).to be_present
-      expect(ActionMailer::Base.deliveries.last.to).to contain_exactly @current_user.email
-    end
+  it "gives you an confirmation mail" do
+    expect(ActionMailer::Base.deliveries.last).to be_present
+    expect(ActionMailer::Base.deliveries.last.to).to contain_exactly @current_user.email
+  end
 
-    it "enable you to confirm" do
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response.status).to eq(302)
+  it "forbids you to login before confirmation and reset password" do
+    login
+    expect(response).to have_http_status(:unauthorized)
+  end
+
+  it "#{expired ? "forbids" : "enables"} you to confirm and login #{expired ? "after a month" : ""}" do
+    travel 32.day if expired
+
+    confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
+      .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
+    get confirmation_link
+    expect(response).to have_http_status(:found)
+    unless expired
       expect(response.location).to include "https://testapp.com/registration", "access-token", "client", "uid"
       expect(response.location).not_to include "reset_password_token"
     end
 
-    it "enable you to confirm and sign out and sign in" do
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response.status).to eq(302)
+    delete destroy_user_session_path, params: Rack::Utils.parse_query(URI.parse(response.location).query)
+    expect(response).to have_http_status(expired == false ? :success : :not_found)
 
-      delete destroy_user_session_path, params: Rack::Utils.parse_query(URI.parse(response.location).query)
-      expect(response.status).to eq(200)
+    login
+    expect(response).to have_http_status(expired == false ? :success : :unauthorized)
+  end if with_pw
 
-      login
-      expect(response.status).to eq(200)
-    end
-  end
+  it "#{expired ? "forbids" : "enables"} you to confirm and login #{expired ? "after a month" : ""}" do
+    travel 32.day if expired
 
-  context "and confimation_token is expired" do
-    it "gives you an new user" do
-      expect(response).to have_http_status(:success)
-    end
+    confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
+      .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
+    get confirmation_link
 
-    it "gives you an confirmation mail" do
-      expect(ActionMailer::Base.deliveries.last).to be_present
-      expect(ActionMailer::Base.deliveries.last.to).to contain_exactly @current_user.email
-    end
-
-    it "forbid you to confirm after a month" do
-      travel 32.day
-
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response.status).to eq(302)
-    end
-
-    it "forbid you to confirm and login after a month" do
-      travel 32.day
-
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response.status).to eq(302)
-
-      delete destroy_user_session_path, params: Rack::Utils.parse_query(URI.parse(response.location).query)
-      expect(response.status).to eq(404)
-
-      login
-      expect(response.status).to eq(401)
-    end
-  end
-end
-
-describe 'Sign up without password', type: :request do
-  before(:each) do
-    @current_user = FactoryBot.build_stubbed(:user)
-    params = {
-      email: @current_user.email,
-      confirm_success_url: "https://testapp.com/registration"
-    }
-    post(user_registration_path, params: params)
-  end
-
-  context "and confirm and sign in" do
-    it "gives you an new user" do
-      expect(response).to have_http_status(:success)
-    end
-
-    it "gives you an confirmation mail" do
-      expect(ActionMailer::Base.deliveries.last).to be_present
-      expect(ActionMailer::Base.deliveries.last.to).to contain_exactly @current_user.email
-    end
-
-    it "forbids you to login before confirmation and reset password" do
-      login
-      expect(response).to have_http_status(:unauthorized)
-    end
-
-    it "enables you to confirm" do
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response).to have_http_status(:found)
+    expect(response).to have_http_status(:found)
+    if expired
+      expect(response.location).to include "https://testapp.com/registration", "account_confirmation_success=false"
+      expect(response.location).not_to include "access-token", "client", "uid", "reset_password_token"
+    else
       expect(response.location).to include "https://testapp.com/registration", "reset_password_token"
       expect(response.location).not_to include "access-token", "client", "uid"
     end
 
-    it "enables you to confirm and reset password" do
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response).to have_http_status(:found)
+    params = Rack::Utils.parse_query(URI.parse(response.location).query).merge({
+      "password"=> "new-password",
+      "password_confirmation"=> "new-password",
+    })
+    patch user_password_path(params)
+    expect(response).to have_http_status(expired == false ? :success : :unauthorized)
 
-      params = Rack::Utils.parse_query(URI.parse(response.location).query).merge({
-        "password"=> "new-password",
-        "password_confirmation"=> "new-password",
-      })
-      patch user_password_path(params)
-      expect(response).to have_http_status(:success)
-    end
+    login "new-password"
+    expect(response).to have_http_status(expired == false ? :success : :unauthorized)
+  end unless with_pw
+end
 
-    it "enables you to confirm and reset password and login" do
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response).to have_http_status(:found)
+RSpec.describe "Sign up with password and confirmation in time, shared_examples" do
+  include_examples "A Ruby object with 4 elements", true, false
+end
 
-      params = Rack::Utils.parse_query(URI.parse(response.location).query).merge({
-        "password"=> "new-password",
-        "password_confirmation"=> "new-password",
-      })
-      patch user_password_path(params)
-      expect(response).to have_http_status(:success)
+RSpec.describe "Sign up with password and confirmation expired, shared_examples" do
+  include_examples "A Ruby object with 4 elements", true, true
+end
 
-      login "new-password"
-      expect(response).to have_http_status(:success)
-    end
-  end
+RSpec.describe "Sign up without password and confirmation in time, shared_examples" do
+  include_examples "A Ruby object with 4 elements", false, false
+end
 
-  context "and confimation_token is expired" do
-    it "forbids you to confirm and reset password and login" do
-      travel 32.day
-
-      confirmation_link = %r{<a href="http://(.+)">Confirm my account<\/a>}
-        .match(ActionMailer::Base.deliveries.last.body.to_s)[1]
-      get confirmation_link
-      expect(response).to have_http_status(:found)
-      expect(response.location).not_to include "reset_password_token"
-
-      params = Rack::Utils.parse_query(URI.parse(response.location).query).merge({
-        "password"=> "new-password",
-        "password_confirmation"=> "new-password",
-      })
-      patch user_password_path(params)
-      expect(response).to have_http_status(:unauthorized)
-
-      login "new-password"
-      expect(response).to have_http_status(:unauthorized)
-    end
-  end
+RSpec.describe "Sign up without password and confirmation expired, shared_examples" do
+  include_examples "A Ruby object with 4 elements", false, true
 end
 
 RSpec.describe 'Whether access is ocurring properly', type: :request do
